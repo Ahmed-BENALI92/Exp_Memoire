52300ed52ad65b63f8f745bc3fb95c8d7f955b36
Ahmed-Benali92 <37007849@parisnanterre.fr>
2022-06-23 21:03:26 +0200

    Add gitclone.py

--- Main.sh
+++ Main.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-
+# La fonction va utiliser l'api github et mettre tous les resultats dans le fichier Resultat_git.json
 function initSearchGit()
 {
-    curl "https://api.github.com/search/repositories?q=src+in:app+language:js">Test.json
+    curl "https://api.github.com/search/repositories?q=src+in:app+language:js">Resultat_git.json
 }
+
+
+
+
 initSearchGit
+
+# Clonner le dossier github via notre code python 
+
+python gitclone.py
 exit
